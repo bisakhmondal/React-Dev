aa123b8d9ccf456984ea566f1f604902f8b356a2
Bisakh Mondal <bisakhmondal00@gmail.com>
2020-02-21 21:04:21 +0530

    Updates

---
+++ precache-manifest.1b2b9f0de93bf42d9c65cae63406b698.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24179fd6267ee9597e4a3e6c3634317b",
+    "url": "/React-Dev/index.html"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/React-Dev/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "58091473646a7ab9966c",
+    "url": "/React-Dev/static/css/main.417a3d52.chunk.css"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/React-Dev/static/js/2.57b720db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Dev/static/js/2.57b720db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58091473646a7ab9966c",
+    "url": "/React-Dev/static/js/main.d1a722ea.chunk.js"
+  },
+  {
+    "revision": "51a376d5adf2aab39db6",
+    "url": "/React-Dev/static/js/runtime-main.0d8469c5.js"
+  }
+]);
